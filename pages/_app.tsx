d9de1b5918a593cef2b552fd16a21738fa467fb4
fujisaki <>
2022-10-08 22:01:16 +0900

    next.js用の設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import React, { createContext, useEffect } from "react";
 import { Provider } from "react-redux";
@@ -5,12 +6,24 @@ import { Auth } from "../auth/Auth";
 import { store } from "../redux/store";
 import "../styles/globals.css";
 import { LoginForm } from "../Components/LoginForm";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, ThemeProvider } from "@mui/material";
 import { useAppDispatch } from "../redux/hooks";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../theme";
+import createEmotionCache from "../createEmotionCache";
 
 export const TokenContext = createContext("");
+const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
   const [isLoading, setIsLoading] = React.useState(true);
   const [loginRequired, setLoginRequired] = React.useState(true);
   const [auth] = React.useState(new Auth());
@@ -48,26 +61,30 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [loginRequired]);
 
   return (
-    <Provider store={store}>
-      <TokenContext.Provider value={token}>
-        {isLoading && (
-          <div className="w-screen h-screen flex justify-center items-center ">
-            <CircularProgress className="[zoom:1.5]" />
-          </div>
-        )}
-        {!isLoading && (
-          <>
-            {loginRequired && (
-              <LoginForm
-                onComplete={handleFormComplete}
-                auth={auth}
-              ></LoginForm>
+    <CacheProvider value={emotionCache!}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <TokenContext.Provider value={token}>
+            {isLoading && (
+              <div className="w-screen h-screen flex justify-center items-center ">
+                <CircularProgress className="[zoom:1.5]" />
+              </div>
             )}
-            {!loginRequired && <Component {...pageProps} />}
-          </>
-        )}
-      </TokenContext.Provider>
-    </Provider>
+            {!isLoading && (
+              <>
+                {loginRequired && (
+                  <LoginForm
+                    onComplete={handleFormComplete}
+                    auth={auth}
+                  ></LoginForm>
+                )}
+                {!loginRequired && <Component {...pageProps} />}
+              </>
+            )}
+          </TokenContext.Provider>
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
