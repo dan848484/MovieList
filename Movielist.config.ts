c98c26656bcb2586c5cf1827ae70113a36577d81
fujisaki <>
2023-03-25 23:04:12 +0900

    envファイルを活用

--- Movielist.config.ts
+++
@@ -1,9 +0,0 @@
-const production = true;
-const endpoint = production
-  ? "https://6biap2l5uh.execute-api.ap-northeast-1.amazonaws.com/Prod/"
-  : "http://127.0.0.1:3850";
-
-const config = {
-  apiEndPoint: endpoint,
-};
-export default config;
